b7deba1be2606ef4dc45c28cf55e13ae16abd64c
mustafa syed <syedmustafaa1@gmail.com>
2020-04-15 14:49:35 -0400

    End of Section 3

--- DatingApp.Api/Controllers/ValuesController.cs
+++ DatingApp.Api/Controllers/ValuesController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -27,6 +28,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
